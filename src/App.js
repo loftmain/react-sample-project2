892cff6a918e6e88919709b87a027b8688a98925
jinjaelee <jjlee009@newsystech.co.kr>
2024-12-16 11:34:35 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
+import { useRef, useState } from "react";
 import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
 
+const mockTodo  = [
+  {
+    id: 0,
+    idDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    idDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    idDone: false,
+    content: "노래 연습",
+    createdDate: new Date().getTime(),
+  },
+];
+
 function App() {
+  const idRef = useRef(3);
+  const [todo, setTodo] = useState(mockTodo);
+
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
   return (
     <div className="App">
       <Header />
-      <TodoEditor />
+      <TodoEditor onCreate={onCreate} />
       <TodoList />
     </div>
   );
